cc5dfa7464ff3d2910373ebd51040b2c71e983a0
r0usta-ctu <levderos@cvut.cz>
2026-01-13 17:48:46 +0100

    Add unit test for `MyFilter` class and include test pretrained model

---
+++ tests/test_filter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Tests for MyFilter class."""
+
+import unittest
+import tests.tst_filterbase
+
+
+class MyFilterTest(tests.tst_filterbase.BaseFilterTestCase):
+    
+    def setUp(self):
+        super().setUp()
+        # Set an instance of class MyFilter for the test
+        from filter import MyFilter
+        self.filter = MyFilter()
+        self.filter.MODEL_PATH = "model/test_pretrained_model.pkl"
+
+       
+if __name__ == '__main__':
+    unittest.main()
